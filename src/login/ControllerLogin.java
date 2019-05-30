76a55948a1646fa06f8ed0b26def45b511c9d6f8
Mehmet Kizil <mehmet_kizil@hotmail.de>
2019-05-30 15:42:04 +0200

    Improved MVC pattern

--- src/login/ControllerLogin.java
+++ src/login/ControllerLogin.java
@@ -3,21 +3,22 @@ package login;
 import application.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class ControllerLogin {
 	
-	@FXML private Button loginButton;
+	ModelLogin model = new ModelLogin();
 	@FXML private TextField userNameField;
 	@FXML private PasswordField passwordField;
 	
-	
-	
+	@FXML	
 	public void login(ActionEvent event) {
-		if (userNameField.getText().equals("test") && passwordField.getText().equals("1234")) {
+		model.setUserName(userNameField.getText());
+		model.setPassword(passwordField.getText());
+		
+		if (model.getUserName().equals("test") && model.getPassword().equals("1234")) {
 			Stage stage = new Stage();
 			Main startUI = new Main();
 			
@@ -26,8 +27,8 @@ public class ControllerLogin {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
-			
+		}else {
+			System.out.println("Try again");
 		}
 	}
 }
